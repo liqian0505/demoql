8e25f51da6434d09df95cd83e9abcf7939c977de
Chun <caochun@gmail.com>
2021-02-10 16:24:54 +0800

    update

--- src/main/java/com/cvicse/demo/repository/RiskRepository.java
+++ src/main/java/com/cvicse/demo/repository/RiskRepository.java
@@ -2,8 +2,11 @@ package com.cvicse.demo.repository;
 
 import com.cvicse.demo.model.Risk;
 
+import org.apache.tomcat.util.http.fileupload.MultipartStream.ItemInputStream;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RiskRepository extends CrudRepository<Risk, String> {
     public Iterable<Risk> findByContractIdOrderByDateDesc(String contractId);
+    
+    // public Iterable<Risk> findByLevelGroupedByContractId
 }
